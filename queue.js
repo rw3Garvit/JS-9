25d53cbf11cde3305df9d508ffb3d015c69372c2
Garvit Dhameliya <rw3.garvit.rd@gmail.com>
2024-02-02 09:47:32 +0530

    que complate

---
+++ queue.js
@@ -0,0 +1,51 @@
+class Q {
+    constructor() {
+        this.que = []
+        this.size = 5
+        this.head = 0
+        this.tail = this.que.length - 1
+    }
+
+
+    isEmpty() {
+        if (this.que.length === 0) console.log("Queue Empty")
+    }
+
+    add(element) {
+
+        if (this.que.length < this.size) {
+            this.que.push(element)
+        }
+        else {
+            console.log("que full");
+        }
+
+    }
+
+
+    print() {
+        console.log(this.que)
+    }
+
+
+    delete() {
+        this.que.shift()
+    }
+
+
+
+
+}
+
+
+
+const que = new Q()
+// que.isEmpty()
+que.add(5)
+que.add(7)
+que.add(8)
+
+
+
+
+que.print()
